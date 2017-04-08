caf522d5d032da2c454eeb6de283723f7fe77401
castusinfo <udaykatikala@gmail.com>
2017-04-07 19:58:30 -0700

    time check class added

--- GetParkDetails/src/com/od/ODGetParkDetailsBL.java
+++ GetParkDetails/src/com/od/ODGetParkDetailsBL.java
@@ -13,6 +13,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.cis.CISConstants;
 import com.cis.CISResults;
+import com.cis.TimeCheck;
 import com.cis.testServiceTime;
 
 
@@ -24,12 +25,9 @@ public class ODGetParkDetailsBL {
 
 	public CISResults getParksDetails(String parkId) {
 		// Capture service Start time
-			  testServiceTime seriveTimeCheck=new testServiceTime();
-			  Calendar current = Calendar.getInstance();
-			  DateFormat formatterTime = new SimpleDateFormat(CISConstants.DATE_FORMAT);
-			  TimeZone objTime = TimeZone.getTimeZone(CISConstants.TIME_ZONE);
-			  formatterTime.setTimeZone( objTime);
-			  String serviceStartTime=formatterTime.format(current.getTime());
+				TimeCheck time=new TimeCheck();
+				 testServiceTime seriveTimeCheck=new testServiceTime();
+				 String serviceStartTime=time.getTimeZone();
 		 			
 			 final Logger logger = Logger.getLogger(ODGetParkInfoBL.class);
 			 CISResults cisResult = parkDetailsDAO.getParksDetails( parkId);
@@ -38,13 +36,10 @@ public class ODGetParkDetailsBL {
 			 
 			 logger.debug("OD GetParkInfoBL service" +cisResult);
 			 
-			// Capture Service End time
-			  Calendar ServiceEnd= Calendar.getInstance();
-		      DateFormat formatter1 = new SimpleDateFormat(CISConstants.DATE_FORMAT);
-		      TimeZone obj1 = TimeZone.getTimeZone(CISConstants.TIME_ZONE);
-		      formatter1.setTimeZone(obj1);
-			  String serviceEndTime=formatter1.format(ServiceEnd.getTime());
-			  seriveTimeCheck.getServiceTime(serviceEndTime,serviceStartTime);
+			 String serviceEndTime=time.getTimeZone();
+			  long result=seriveTimeCheck.getServiceTime(serviceEndTime,serviceStartTime);
+			  logger.info("Database time for login service:: " +result );
+			 // seriveTimeCheck.getServiceTime(serviceEndTime,serviceStartTime);
 			  
 			 return cisResult;
 		}
