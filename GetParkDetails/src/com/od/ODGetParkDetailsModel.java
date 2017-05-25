29f04470be5bdb1103af8ba0e8024e42673c603b
castus <castus@castus-PC>
2017-05-24 20:31:25 -0700

    model changes

--- GetParkDetails/src/com/od/ODGetParkDetailsModel.java
+++ GetParkDetails/src/com/od/ODGetParkDetailsModel.java
@@ -10,7 +10,7 @@ public class ODGetParkDetailsModel {
 	public String faq;
 	public String photoUrl;
 	public String name;
-	public float odRating;
+	public Float odRating;
 	public String metro ;
 	public String localArea;
 	public String streetAddress;
@@ -78,69 +78,19 @@ public class ODGetParkDetailsModel {
 	}
 	
 	
-	
-
-	public String getParkId() {
-		return parkId;
+	public String getAmenitiesString() {
+		return amenitiesString;
 	}
 
 
-
-
-	public void setParkId(String parkId) {
-		this.parkId = parkId;
+	public void setAmenitiesString(String amenitiesString) {
+		this.amenitiesString = amenitiesString;
 	}
-
-
-
-
-	public String getOverviewText() {
-		return overviewText;
-	}
-
-
-
-
-	public void setOverviewText(String overviewText) {
-		this.overviewText = overviewText;
-	}
-
-
-
-
-	public String getFaq() {
-		return faq;
-	}
-
-
-
-
-	public void setFaq(String faq) {
-		this.faq = faq;
-	}
-
-
-
-
-	public String getPhotoUrl() {
-		return photoUrl;
-	}
-
-
-
-
-	public void setPhotoUrl(String photoUrl) {
-		this.photoUrl = photoUrl;
-	}
-
-
 
 
 	public String getName() {
 		return name;
 	}
-
-
 
 
 	public void setName(String name) {
@@ -149,110 +99,9 @@ public class ODGetParkDetailsModel {
 
 
 
-
-	public Float getOdRating() {
-		return odRating;
-	}
-
-
-
-
-	public void setOdRating(Float odRating) {
-		this.odRating = odRating;
-	}
-
-
-
-
-	public String getMetro() {
-		return metro;
-	}
-
-
-
-
-	public void setMetro(String metro) {
-		this.metro = metro;
-	}
-
-
-
-
-	public String getLocalArea() {
-		return localArea;
-	}
-
-
-
-
-	public void setLocalArea(String localArea) {
-		this.localArea = localArea;
-	}
-
-
-
-
-	public String getStreetAddress() {
-		return streetAddress;
-	}
-
-
-
-
-	public void setStreetAddress(String streetAddress) {
-		this.streetAddress = streetAddress;
-	}
-
-
-
-
-	public String getCity() {
-		return city;
-	}
-
-
-
-
-	public void setCity(String city) {
-		this.city = city;
-	}
-
-
-
-
-	public String getState() {
-		return state;
-	}
-
-
-
-
-	public void setState(String state) {
-		this.state = state;
-	}
-
-
-
-
-	public int getPin() {
-		return pin;
-	}
-
-
-
-
-	public void setPin(int pin) {
-		this.pin = pin;
-	}
-
-
-
-
 	public float getLatitude() {
 		return latitude;
 	}
-
-
 
 
 	public void setLatitude(float latitude) {
@@ -260,13 +109,9 @@ public class ODGetParkDetailsModel {
 	}
 
 
-
-
 	public float getLongitude() {
 		return longitude;
 	}
-
-
 
 
 	public void setLongitude(float longitude) {
@@ -274,13 +119,9 @@ public class ODGetParkDetailsModel {
 	}
 
 
-
-
 	public String getTypeString() {
 		return typeString;
 	}
-
-
 
 
 	public void setTypeString(String typeString) {
@@ -288,13 +129,9 @@ public class ODGetParkDetailsModel {
 	}
 
 
-
-
 	public String getNatureString() {
 		return natureString;
 	}
-
-
 
 
 	public void setNatureString(String natureString) {
@@ -302,13 +139,9 @@ public class ODGetParkDetailsModel {
 	}
 
 
-
-
 	public String getSubCategoryString() {
 		return subCategoryString;
 	}
-
-
 
 
 	public void setSubCategoryString(String subCategoryString) {
@@ -316,13 +149,9 @@ public class ODGetParkDetailsModel {
 	}
 
 
-
-
 	public String getDurationString() {
 		return durationString;
 	}
-
-
 
 
 	public void setDurationString(String durationString) {
@@ -330,13 +159,9 @@ public class ODGetParkDetailsModel {
 	}
 
 
-
-
 	public String getMaxPeople() {
 		return maxPeople;
 	}
-
-
 
 
 	public void setMaxPeople(String maxPeople) {
@@ -344,13 +169,9 @@ public class ODGetParkDetailsModel {
 	}
 
 
-
-
 	public String getPopularity() {
 		return popularity;
 	}
-
-
 
 
 	public void setPopularity(String popularity) {
@@ -358,20 +179,14 @@ public class ODGetParkDetailsModel {
 	}
 
 
-
-
-	public float getMinCost1() {
+	public Float getMinCost1() {
 		return minCost1;
 	}
 
 
-
-
-	public void setMinCost1(float minCost1) {
+	public void setMinCost1(Float minCost1) {
 		this.minCost1 = minCost1;
 	}
-
-
 
 
 	public int getMinCost1People() {
@@ -379,34 +194,9 @@ public class ODGetParkDetailsModel {
 	}
 
 
-
-
 	public void setMinCost1People(int minCost1People) {
 		this.minCost1People = minCost1People;
 	}
-
-
-
-
-	public Float getMinCost2() {
-		return minCost2;
-	}
-
-
-
-
-	public void setMinCost2(Float minCost2) {
-		this.minCost2 = minCost2;
-	}
-
-
-
-
-	public int getMinCost2People() {
-		return minCost2People;
-	}
-
-
 
 
 	public void setMinCost2People(int minCost2People) {
@@ -414,34 +204,34 @@ public class ODGetParkDetailsModel {
 	}
 
 
+	public Float getMinCost2() {
+		return minCost2;
+	}
 
 
-	public float getSpecialOfferPercentage() {
+	public void setMinCost2(Float minCost2) {
+		this.minCost2 = minCost2;
+	}
+
+
+	public Float getSpecialOfferPercentage() {
 		return specialOfferPercentage;
 	}
 
 
-
-
-	public void setSpecialOfferPercentage(float specialOfferPercentage) {
+	public void setSpecialOfferPercentage(Float specialOfferPercentage) {
 		this.specialOfferPercentage = specialOfferPercentage;
 	}
 
 
-
-
-	public float getSpecialRate() {
+	public Float getSpecialRate() {
 		return specialRate;
 	}
 
 
-
-
-	public void setSpecialRate(float specialRate) {
+	public void setSpecialRate(Float specialRate) {
 		this.specialRate = specialRate;
 	}
-
-
 
 
 	public String getDescription() {
@@ -449,13 +239,9 @@ public class ODGetParkDetailsModel {
 	}
 
 
-
-
 	public void setDescription(String description) {
 		this.description = description;
 	}
-
-
 
 
 	public String getDetails() {
@@ -463,20 +249,13 @@ public class ODGetParkDetailsModel {
 	}
 
 
-
-
 	public void setDetails(String details) {
 		this.details = details;
 	}
 
-
-
-
 	public String getParkSubImages() {
 		return parkSubImages;
 	}
-
-
 
 
 	public void setParkSubImages(String parkSubImages) {
@@ -484,20 +263,118 @@ public class ODGetParkDetailsModel {
 	}
 
 
-
-
-	public String getAmenitiesString() {
-		return amenitiesString;
+	public int getMinCost2People() {
+		return minCost2People;
 	}
 
 
-
-
-	public void setAmenitiesString(String amenitiesString) {
-		this.amenitiesString = amenitiesString;
+	public String getMetro() {
+		return metro;
 	}
 
 
+	public void setMetro(String metro) {
+		this.metro = metro;
+	}
+
+
+	public String getLocalArea() {
+		return localArea;
+	}
+
+
+	public void setLocalArea(String localArea) {
+		this.localArea = localArea;
+	}
+
+
+	public String getStreetAddress() {
+		return streetAddress;
+	}
+
+
+	public void setStreetAddress(String streetAddress) {
+		this.streetAddress = streetAddress;
+	}
+
+
+	public String getCity() {
+		return city;
+	}
+
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+
+	public String getState() {
+		return state;
+	}
+
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public Float getOdRating() {
+		return odRating;
+	}
+
+
+	public void setOdRating(Float odRating) {
+		this.odRating = odRating;
+	}
+
+
+	public int getPin() {
+		return pin;
+	}
+
+
+	public void setPin(int pin) {
+		this.pin = pin;
+	}
+
+
+	public String getParkId() {
+		return parkId;
+	}
+
+
+	public void setParkId(String parkId) {
+		this.parkId = parkId;
+	}
+
+
+	public String getOverviewText() {
+		return overviewText;
+	}
+
+
+	public void setOverviewText(String overviewText) {
+		this.overviewText = overviewText;
+	}
+
+
+	public String getFaq() {
+		return faq;
+	}
+
+
+	public void setFaq(String faq) {
+		this.faq = faq;
+	}
+
+
+	public String getPhotoUrl() {
+		return photoUrl;
+	}
+
+
+	public void setPhotoUrl(String photoUrl) {
+		this.photoUrl = photoUrl;
+	}
 
 
 	public List<ODParkDetailsService> getParkDetails() {
@@ -505,13 +382,9 @@ public class ODGetParkDetailsModel {
 	}
 
 
-
-
 	public void setParkDetails(List<ODParkDetailsService> parkDetails) {
 		this.parkDetails = parkDetails;
 	}
-
-
 
 
 	public ODGetParkDetailsModel(){
--- ODGetParksInfo/src/com/od/ODGetParkInfoModel.java
+++ ODGetParksInfo/src/com/od/ODGetParkInfoModel.java
@@ -83,376 +83,313 @@ public class ODGetParkInfoModel {
 		this.tagName=tagName;
 	}
 		
-		
-		public String getAppId() {
-		return appId;
-	}
-
-
-	public void setAppId(String appId) {
-		this.appId = appId;
-	}
-
-
-	public String getParktype() {
-		return parktype;
-	}
-
-
-	public void setParktype(String parktype) {
-		this.parktype = parktype;
-	}
-
-
-	public String getParkSubcategory() {
-		return parkSubcategory;
-	}
-
-
-	public void setParkSubcategory(String parkSubcategory) {
-		this.parkSubcategory = parkSubcategory;
-	}
-
-
-	public String getMetro() {
-		return metro;
-	}
-
-
-	public void setMetro(String metro) {
-		this.metro = metro;
-	}
-
-
-	public String getLocalArea() {
-		return localArea;
-	}
-
-
-	public void setLocalArea(String localArea) {
-		this.localArea = localArea;
-	}
-
-
-	public String getStreetAddress() {
-		return streetAddress;
-	}
-
-
-	public void setStreetAddress(String streetAddress) {
-		this.streetAddress = streetAddress;
-	}
-
-
-	public String getCity() {
-		return city;
-	}
-
-
-	public void setCity(String city) {
-		this.city = city;
-	}
-
-
-	public String getState() {
-		return state;
-	}
-
-
-	public void setState(String state) {
-		this.state = state;
-	}
-
-
-	public String getPin() {
-		return pin;
-	}
-
-
-	public void setPin(String pin) {
-		this.pin = pin;
-	}
-
-
-	public String getName() {
-		return name;
-	}
-
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-
-	public String getDescription() {
-		return description;
-	}
-
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-
-	public String getDetails() {
-		return details;
-	}
-
-
-	public void setDetails(String details) {
-		this.details = details;
-	}
-
-
-	public int getParkId() {
-		return parkId;
-	}
-
-
-	public void setParkId(int parkId) {
-		this.parkId = parkId;
-	}
-
-
-	public float getOdRating() {
-		return odRating;
-	}
-
-
-	public void setOdRating(float odRating) {
-		this.odRating = odRating;
-	}
-
-
-	public String getSponsoredFeaturedFlag() {
-		return sponsoredFeaturedFlag;
-	}
-
-
-	public void setSponsoredFeaturedFlag(String sponsoredFeaturedFlag) {
-		this.sponsoredFeaturedFlag = sponsoredFeaturedFlag;
-	}
-
-
-	public String getOverviewText() {
-		return overviewText;
-	}
-
-
-	public void setOverviewText(String overviewText) {
-		this.overviewText = overviewText;
-	}
-
-
-	public String getFaq() {
-		return faq;
-	}
-
-
-	public void setFaq(String faq) {
-		this.faq = faq;
-	}
-
-
-	public float getLatitude() {
-		return latitude;
-	}
-
-
-	public void setLatitude(float latitude) {
-		this.latitude = latitude;
-	}
-
-
-	public float getLongitude() {
-		return longitude;
-	}
-
-
-	public void setLongitude(float longitude) {
-		this.longitude = longitude;
-	}
-
-
-	public String getTypeString() {
-		return typeString;
-	}
-
-
-	public void setTypeString(String typeString) {
-		this.typeString = typeString;
-	}
-
-
-	public String getNatureString() {
-		return natureString;
-	}
-
-
-	public void setNatureString(String natureString) {
-		this.natureString = natureString;
-	}
-
-
-	public String getSubCategoryString() {
-		return subCategoryString;
-	}
-
-
-	public void setSubCategoryString(String subCategoryString) {
-		this.subCategoryString = subCategoryString;
-	}
-
-
-	public String getDurationString() {
-		return durationString;
-	}
-
-
-	public void setDurationString(String durationString) {
-		this.durationString = durationString;
-	}
-
-
-	public String getMaxPeople() {
-		return maxPeople;
-	}
-
-
-	public void setMaxPeople(String maxPeople) {
-		this.maxPeople = maxPeople;
-	}
-
-
-	public Date getDateAdded() {
-		return dateAdded;
-	}
-
-
-	public void setDateAdded(Date dateAdded) {
-		this.dateAdded = dateAdded;
-	}
-
-
-	public String getPopularity() {
-		return popularity;
-	}
-
-
-	public void setPopularity(String popularity) {
-		this.popularity = popularity;
-	}
-
-
-	public float getMinCost1() {
-		return minCost1;
-	}
-
-
-	public void setMinCost1(float minCost1) {
-		this.minCost1 = minCost1;
-	}
-
-
-	public float getMinCost1People() {
-		return minCost1People;
-	}
-
-
-	public void setMinCost1People(float minCost1People) {
-		this.minCost1People = minCost1People;
-	}
-
-
-	public float getMinCost2() {
-		return minCost2;
-	}
-
-
-	public void setMinCost2(float minCost2) {
-		this.minCost2 = minCost2;
-	}
-
-
-	public float getMinCost2People() {
-		return minCost2People;
-	}
-
-
-	public void setMinCost2People(float minCost2People) {
-		this.minCost2People = minCost2People;
-	}
-
-
-	public float getMinCost3() {
-		return minCost3;
-	}
-
-
-	public void setMinCost3(float minCost3) {
-		this.minCost3 = minCost3;
-	}
-
-
-	public float getMinCost3People() {
-		return minCost3People;
-	}
-
-
-	public void setMinCost3People(float minCost3People) {
-		this.minCost3People = minCost3People;
-	}
-
-
-	public float getSpecialOfferPercentage() {
-		return specialOfferPercentage;
-	}
-
-
-	public void setSpecialOfferPercentage(float specialOfferPercentage) {
-		this.specialOfferPercentage = specialOfferPercentage;
-	}
-
-
-	public float getSpecialRate() {
-		return specialRate;
-	}
-
-
-	public void setSpecialRate(float specialRate) {
-		this.specialRate = specialRate;
-	}
-
-
-	public String getPhotoUrl() {
-		return photoUrl;
-	}
-
-
-	public void setPhotoUrl(String photoUrl) {
-		this.photoUrl = photoUrl;
-	}
-
-
-	public String getImagefoldername() {
-		return imagefoldername;
-	}
-
-
-	public void setImagefoldername(String imagefoldername) {
-		this.imagefoldername = imagefoldername;
-	}
-
-
-	public String getTagName() {
+		public String getTagName() {
 		return tagName;
 	}
-
 
 	public void setTagName(String tagName) {
 		this.tagName = tagName;
 	}
 
+		public String getImagefoldername() {
+		return imagefoldername;
+	}
+
+	public void setImagefoldername(String imagefoldername) {
+		this.imagefoldername = imagefoldername;
+	}
+
+		public String getAppId() {
+		return appId;
+	}
+
+	public void setAppId(String appId) {
+		this.appId = appId;
+	}
+
+	public String getParktype() {
+		return parktype;
+	}
+
+	public void setParktype(String parktype) {
+		this.parktype = parktype;
+	}
+
+	
+
+	public String getParkSubcategory() {
+		return parkSubcategory;
+	}
+
+	public void setParkSubcategory(String parkSubcategory) {
+		this.parkSubcategory = parkSubcategory;
+	}
+
+	public String getMetro() {
+		return metro;
+	}
+
+	public void setMetro(String metro) {
+		this.metro = metro;
+	}
+
+	public String getLocalArea() {
+		return localArea;
+	}
+
+	public void setLocalArea(String localArea) {
+		this.localArea = localArea;
+	}
+
+	public String getStreetAddress() {
+		return streetAddress;
+	}
+
+	public void setStreetAddress(String streetAddress) {
+		this.streetAddress = streetAddress;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getPin() {
+		return pin;
+	}
+
+	public void setPin(String pin) {
+		this.pin = pin;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getDetails() {
+		return details;
+	}
+
+	public void setDetails(String details) {
+		this.details = details;
+	}
+
+	
+
+	
+
+	public int getParkId() {
+		return parkId;
+	}
+
+	public void setParkId(int parkId) {
+		this.parkId = parkId;
+	}
+
+	
+
+	public float getOdRating() {
+		return odRating;
+	}
+
+	public void setOdRating(float odRating) {
+		this.odRating = odRating;
+	}
+
+	public String getSponsoredFeaturedFlag() {
+		return sponsoredFeaturedFlag;
+	}
+
+	public void setSponsoredFeaturedFlag(String sponsoredFeaturedFlag) {
+		this.sponsoredFeaturedFlag = sponsoredFeaturedFlag;
+	}
+
+	public String getOverviewText() {
+		return overviewText;
+	}
+
+	public void setOverviewText(String overviewText) {
+		this.overviewText = overviewText;
+	}
+
+	public String getFaq() {
+		return faq;
+	}
+
+	public void setFaq(String faq) {
+		this.faq = faq;
+	}
+
+	
+
+	public float getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(float latitude) {
+		this.latitude = latitude;
+	}
+
+	public float getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(float longitude) {
+		this.longitude = longitude;
+	}
+
+	public String getTypeString() {
+		return typeString;
+	}
+
+	public void setTypeString(String typeString) {
+		this.typeString = typeString;
+	}
+
+	public String getNatureString() {
+		return natureString;
+	}
+
+	public void setNatureString(String natureString) {
+		this.natureString = natureString;
+	}
+
+	public String getSubCategoryString() {
+		return subCategoryString;
+	}
+
+	public void setSubCategoryString(String subCategoryString) {
+		this.subCategoryString = subCategoryString;
+	}
+
+	public String getDurationString() {
+		return durationString;
+	}
+
+	public void setDurationString(String durationString) {
+		this.durationString = durationString;
+	}
+
+	public String getMaxPeople() {
+		return maxPeople;
+	}
+
+	public void setMaxPeople(String maxPeople) {
+		this.maxPeople = maxPeople;
+	}
+
+	public Date getDateAdded() {
+		return dateAdded;
+	}
+
+	public void setDateAdded(Date dateAdded) {
+		this.dateAdded = dateAdded;
+	}
+
+	public String getPopularity() {
+		return popularity;
+	}
+
+	public void setPopularity(String popularity) {
+		this.popularity = popularity;
+	}
+
+	public Float getMinCost1() {
+		return minCost1;
+	}
+
+	public void setMinCost1(Float minCost1) {
+		this.minCost1 = minCost1;
+	}
+
+	public Float getMinCost1People() {
+		return minCost1People;
+	}
+
+	public void setMinCost1People(Float minCost1People) {
+		this.minCost1People = minCost1People;
+	}
+
+	public Float getMinCost2() {
+		return minCost2;
+	}
+
+	public void setMinCost2(Float minCost2) {
+		this.minCost2 = minCost2;
+	}
+
+	public Float getMinCost2People() {
+		return minCost2People;
+	}
+
+	public void setMinCost2People(Float minCost2People) {
+		this.minCost2People = minCost2People;
+	}
+
+	public Float getMinCost3() {
+		return minCost3;
+	}
+
+	public void setMinCost3(Float minCost3) {
+		this.minCost3 = minCost3;
+	}
+
+	public Float getMinCost3People() {
+		return minCost3People;
+	}
+
+	public void setMinCost3People(Float minCost3People) {
+		this.minCost3People = minCost3People;
+	}
+
+	public Float getSpecialOfferPercentage() {
+		return specialOfferPercentage;
+	}
+
+	public void setSpecialOfferPercentage(Float specialOfferPercentage) {
+		this.specialOfferPercentage = specialOfferPercentage;
+	}
+
+	public Float getSpecialRate() {
+		return specialRate;
+	}
+
+	public void setSpecialRate(Float specialRate) {
+		this.specialRate = specialRate;
+	}
+
+	public String getPhotoUrl() {
+		return photoUrl;
+	}
+
+	
+
+	public void setPhotoUrl(String photoUrl) {
+		this.photoUrl = photoUrl;
+	}
 
 		public ODGetParkInfoModel(){
 			
