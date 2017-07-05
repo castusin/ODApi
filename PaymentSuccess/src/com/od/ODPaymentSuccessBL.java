7d54e75f9f33f8d349e223a84364c368685ecf60
castus <castus@castus-PC>
2017-07-05 11:28:07 -0700

    emails changed

--- PaymentSuccess/src/com/od/ODPaymentSuccessBL.java
+++ PaymentSuccess/src/com/od/ODPaymentSuccessBL.java
@@ -36,7 +36,7 @@ public class ODPaymentSuccessBL {
 	    CISResults cisResult= new CISResults();
 		testServiceTime seriveTimeCheck=new testServiceTime();
 		String serviceStartTime=time.getTimeZone();
-		 List<GetDatesModel> dateList = null;		
+		List<GetDatesModel> dateList = null;		
 		final Logger logger = Logger.getLogger(ODPaymentSuccessBL.class);
 	  		 
 	     cisResult = successDAO.paymentSuccess(transactionId,status);
@@ -53,28 +53,26 @@ public class ODPaymentSuccessBL {
 		    }	
 	     
 	     else{
-	   //  if(status.equalsIgnoreCase(CISConstants.STATUS1)){
-	    	 
-	     List<GetRoomDetailsModel> details=successDAO.getRoomDetails(transactionId);
-	     String typecode="";
-	     String title="";
-	     Date checkIn = null;
-	     Date checkOut = null;
-	     int qty=0;
-	     int parkid=0;
-	     float price=0;
-	     float totalPrice=0;
-	     String type="";
-	     String  suppEmail="";
-	     String suppCell="";
-	     String suppLand="";
+	    	 List<GetRoomDetailsModel> details=successDAO.getRoomDetails(transactionId);
+	    	 String typecode="";
+	    	 String title="";
+	    	 Date checkIn = null;
+	    	 Date checkOut = null;
+	    	 int qty=0;
+	    	 int parkid=0;
+	    	 float price=0;
+	    	 float totalPrice=0;
+	    	 String type="";
+	    	 String  suppEmail="";
+	    	 String suppCell="";
+	    	 String suppLand="";
+	     
 			ArrayList<Object> roomDetails = new ArrayList<Object>();
 			 
 			for (int i = 0; i < details.size(); i++) {
 				
 			GetRoomDetailsModel roomDetaisModel = new GetRoomDetailsModel();
-			//Color color = new Color();
-				
+			
 			     typecode= details.get(i).typeCode;
 			     title=details.get(i).title;
 			     type=details.get(i).type;
@@ -84,26 +82,21 @@ public class ODPaymentSuccessBL {
 			     parkid=details.get(i).parkId;
 			     price=details.get(i).price;
 			     totalPrice=details.get(i).totalPrice;
-				
-			    roomDetails.add(roomDetaisModel);
-				
-			}
+				 roomDetails.add(roomDetaisModel);
+				}
 			
-
 			 cisResult.setResultObject(roomDetails);
 			
-	    
-	    	 
 	    	 // get emailId and firstname from db
-	    	cisResult = successDAO.paymentEmail(transactionId);
-			String paymentEmail="";
-			ODPaymentSuccessModel  emailId=(ODPaymentSuccessModel)cisResult.getResultObject();
-			paymentEmail=emailId.getEmailId();
-			String firstName="";
-			ODPaymentSuccessModel  name=(ODPaymentSuccessModel)cisResult.getResultObject();
-			firstName=name.getFirstName();
+			 cisResult = successDAO.paymentEmail(transactionId);
+			 String paymentEmail="";
+			 ODPaymentSuccessModel  emailId=(ODPaymentSuccessModel)cisResult.getResultObject();
+			 paymentEmail=emailId.getEmailId();
+			 String firstName="";
+			 ODPaymentSuccessModel  name=(ODPaymentSuccessModel)cisResult.getResultObject();
+			 firstName=name.getFirstName();
 			
-			cisResult = successDAO.getParkAddress(parkid);
+			 cisResult = successDAO.getParkAddress(parkid);
 			
 			
 			ParkAddress  address=(ParkAddress)cisResult.getResultObject();
@@ -113,36 +106,29 @@ public class ODPaymentSuccessBL {
 			int pin=address.getPin();
 			String parkName=address.getResortname();
 			
-			 List<SupplierModel> suppdetails= successDAO.SupplierEmail(parkid);
-			
-			
-			
+			List<SupplierModel> suppdetails= successDAO.SupplierEmail(parkid);
 			ArrayList<Object> supplierDetails = new ArrayList<Object>();
 			 
 			for (int i = 0; i < suppdetails.size(); i++) {
 				
 				SupplierModel supplierDetaisModel = new SupplierModel();
-			//Color color = new Color();
 				
 				suppEmail= suppdetails.get(i).emailId;
 			    suppCell=suppdetails.get(i).cell;
 				suppLand=suppdetails.get(i).land;
-				
-			     supplierDetails.add(supplierDetaisModel);
+		        supplierDetails.add(supplierDetaisModel);
 				
 			}
-			
-
 			 cisResult.setResultObject(supplierDetails);
 			 
 			 if(cod.equalsIgnoreCase(CISConstants.PAYMENTSUCCESS)){
 			
-			//sending all parameters required	 
-			cisResult=sendMail.sendPaymentstatus(paymentEmail,firstName,checkIn,checkOut,price,transactionId,title,totalPrice,qty,streetAddress,city,state,pin,parkName,suppCell,suppEmail);
+			
+				 cisResult=sendMail.sendPaymentstatus(paymentEmail,firstName,checkIn,checkOut,price,transactionId,title,totalPrice,qty,streetAddress,city,state,pin,parkName,suppCell,suppEmail);
 	    	
-			cisResult=sendMail.sendAdminSuccessMail(firstName,checkIn,checkOut,price,transactionId,title,totalPrice,qty,streetAddress,city,state,pin,parkName,suppCell,suppEmail);
-	    	
-			cisResult=sendMail.sendSupplierSuccessMail(suppEmail,firstName,checkIn,checkOut,price,transactionId,title,totalPrice,qty,streetAddress,city,state,pin,parkName,suppCell);
+				 cisResult=sendMail.sendAdminSuccessMail(firstName,checkIn,checkOut,price,transactionId,title,totalPrice,qty,streetAddress,city,state,pin,parkName,suppCell,suppEmail);
+			
+				 cisResult=sendMail.sendSupplierSuccessMail(suppEmail,firstName,checkIn,checkOut,price,transactionId,title,totalPrice,qty,streetAddress,city,state,pin,parkName,suppCell);
 			}else if(cod.equalsIgnoreCase(CISConstants.COD)){
 				
 				cisResult=sendMail.sendPaymentstatusCOD(paymentEmail,firstName,checkIn,checkOut,price,transactionId,title,totalPrice,qty,streetAddress,city,state,pin,parkName,suppCell,suppEmail);
@@ -150,19 +136,15 @@ public class ODPaymentSuccessBL {
 				cisResult=sendMail.sendAdminSuccessMailCOD(firstName,checkIn,checkOut,price,transactionId,title,totalPrice,qty,streetAddress,city,state,pin,parkName,suppCell,suppEmail);
 		    	
 				cisResult=sendMail.sendSupplierSuccessMailCOD(suppEmail,firstName,checkIn,checkOut,price,transactionId,title,totalPrice,qty,streetAddress,city,state,pin,parkName,suppCell);
-			
 				
 			}
 			
 			
 			// get current availability
 			 cisResult = successDAO.getAvailablility(transactionId);
-			
-			
+		
 			 cisResult = successDAO.getUpdateAvailablility(transactionId);
 		
-	 //  }
-	    
 	     } 
 	     
 			// Capture Service End time
